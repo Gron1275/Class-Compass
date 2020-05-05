01db735ae0d16e5d0441345ae946f91f9b6c6472
Gron1275 <grennongurney@gmail.com>
2020-05-04 22:34:42 -0400

    Moved PointType Enum to seperate file

--- Point.cs
+++ Point.cs
@@ -16,5 +16,4 @@ namespace RecommendationEngine
         }
 
     }
-    public enum PointType { noise = 0, border = 1, core = 2 };
 }
---
+++ PointType.cs
@@ -0,0 +1,7 @@
+﻿namespace RecommendationEngine
+{
+    public enum PointType { noise = 0,
+                            border = 1,
+                            core = 2        
+                            };
+}
