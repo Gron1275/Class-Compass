4a88ed92adba7539bd47aabda45086cd9ca9b0c2
Gron1275 <grennongurney@gmail.com>
2020-05-24 17:13:49 -0400

    Update Point.cs

--- Point.cs
+++ Point.cs
@@ -6,8 +6,7 @@ namespace RecommendationEngine
     public enum PointType
     {
         noise = 0,
-        border = 1, //Actually designate which points are border points the suggestions given to them from their cluster are weighted less than their interests 
-                    //b/c they are less similar so their recommendations should be based more on the item based part of the algo than the user similarity
+        border = 1,
         core = 2
     };
     public class Point
