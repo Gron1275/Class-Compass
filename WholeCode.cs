f583a4c2e2cae08001a37285082d3b73f9f165b3
Gron1275 <grennongurney@gmail.com>
2020-05-24 16:16:36 -0400

    Update WholeCode.cs

--- WholeCode.cs
+++ WholeCode.cs
@@ -187,11 +187,31 @@ namespace wholeCodeNamespace
             this.timeElapsed = stopwatch.ElapsedMilliseconds * 0.001;
             OnDBScanFinished(this.points);
         }
+        double DistanceTo(Point xPoint, Point yPoint)
+        {
+            double sigma = 0.0;
+            for (int i = 0; i < this.FeatureArray.Length; i++)
+            {
+                sigma += Math.Pow(xPoint.FeatureArray[i] - yPoint.FeatureArray[i], 2);
+            }
+            double distance = Math.Sqrt(sigma);
+            return distance;
+        }
+        List<Point> findNeighborsOf(Point queryPoint)
+        {
+            List<Point> tempList = new List<Point>();
+            foreach (Point currentPoint in this.points)
+            {
+                if (DistanceTo(queryPoint, currentPoint) <= this.epsilon)
+                {
+                    tempList.Add(currentPoint);
+                }
+            }
+        }
         bool ExpandCluster(ref Point point, int ClusterID)
         {
             List<Point> epsilonNeighborhood = new List<Point>();
-
-            List<Point> findNeighborsOf(Point queryPoint) => this.points.Where(currentPoint => currentPoint.DistanceTo(queryPoint) <= this.epsilon).ToList();
+            
             epsilonNeighborhood = findNeighborsOf(point);
 
             if (epsilonNeighborhood.Count < this.minNeighbor)
